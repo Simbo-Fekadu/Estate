dfd6a80ebc1bc7b93db514c11d51a357e04efee3
Simbo Fekadu <simbofekadu2629@gmail.com>
2023-12-31 16:49:14 +0300

    connecting to the mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(process.env.mymongo).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 app.listen(3000, () => {
-    console.log("server running on port 3000")
-})
+    console.log("server running on port 3000");
+});
