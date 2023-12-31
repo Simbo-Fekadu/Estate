e1fc9a35e05c2bfcfe75f8ba2fcac31012581428
Simbo Fekadu <simbofekadu2629@gmail.com>
2023-12-31 11:41:05 +0300

    framework installed and environment setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-red-500">HOLA!</h2>
+    </div>
+  )
+}
+
+export default App
