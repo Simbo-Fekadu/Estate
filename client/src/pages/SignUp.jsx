3ba0f8b8a8b0f58db34176e94b1ec20950bef8a5
Simbo Fekadu <simbofekadu2629@gmail.com>
2024-01-05 15:34:58 +0300

    Sign in page full

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -74,7 +74,7 @@ const SignUp = () => {
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to={"./sign-in"}>
+        <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
