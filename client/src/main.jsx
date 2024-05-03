5f04c22e7b8902b11c03c5e8c694071ca18f623c
Simbo Fekadu <simbofekadu2629@gmail.com>
2024-05-03 15:05:54 +0300

    some adjustment on react redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
-import PersistGate from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
